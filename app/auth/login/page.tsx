b87a87a250d7d8315e2542286faa486d2c48d468
tebus- <villeterasahde@gmail.com>
2026-01-18 21:47:35 +0200

    Redirect to root from login page if already logged in

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,16 @@
 import LoginForm from "@/components/forms/auth/LoginForm";
+import { getUser } from "@/services/supabase/auth/user";
+import { redirect } from "next/navigation";
 
-const Login = () => {
+async function Login() {
+  const user = await getUser();
+  if (user) redirect("/");
+
   return (
     <>
       <LoginForm />
     </>
   );
-};
+}
 
 export default Login;
