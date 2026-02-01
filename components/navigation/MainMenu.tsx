de87588a91f6ad729c9022e906c92924d64bc2e7
tebus- <villeterasahde@gmail.com>
2026-02-01 23:31:04 +0200

    menu fix

--- components/navigation/MainMenu.tsx
+++ components/navigation/MainMenu.tsx
@@ -1,18 +1,17 @@
+import { getUser } from "@/services/supabase/auth/server";
 import { Button, Flex, HStack, Link } from "@chakra-ui/react";
 import { LogoutButton } from "../common/auth/LogoutButton";
 import RestrictedRender from "../common/auth/RestrictedRender";
 
 async function MainMenu() {
+  const user = await getUser();
   return (
     <Flex gap={2} m={2} justify={"space-between"}>
       <Button p={5} colorPalette={"black"} variant={"outline"}>
         <Link href="/">Etusivu</Link>
       </Button>
       <RestrictedRender>
-        <HStack>
-          {/* <PlayerGroupSelection /> */}
-          <LogoutButton />
-        </HStack>
+        <HStack>{user && <LogoutButton />}</HStack>
       </RestrictedRender>
     </Flex>
   );
